a11168fc0e3e4b8ddfdbe1a03e6419dc8ddd6c5a
wangdengfeng <1213411503@qq.com>
2018-01-15 18:10:35 +0800

    增加菜单

--- app/src/main/java/com/wonderful/framework/activity/main/MainActivity.java
+++ app/src/main/java/com/wonderful/framework/activity/main/MainActivity.java
@@ -2,9 +2,7 @@ package com.wonderful.framework.activity.main;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
 
 import com.wonderful.framework.R;
 import com.wonderful.framework.base.BaseActivity;
@@ -12,18 +10,13 @@ import com.wonderful.framework.base.BaseActivity;
 import butterknife.BindView;
 
 public class MainActivity extends BaseActivity {
-    @BindView(R.id.tvTest)
-    TextView tvTest;
+
+    @BindView(R.id.rvMenu)
+    RecyclerView rvMenu;
 
     public static void actionStart(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
         context.startActivity(intent);
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -32,17 +25,8 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    protected boolean isImmersionBarEnabled() {
-        return false;
-    }
-
-    @Override
     protected void initViews() {
-        tvTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+
     }
 
     @Override
@@ -59,4 +43,5 @@ public class MainActivity extends BaseActivity {
     protected void loadData() {
 
     }
+
 }
